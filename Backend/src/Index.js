79a17e83d2fc2b5cd64df5ff98d42934b20365e4
Maximo <Maximokugler9@gmail.com>
2025-10-09 20:46:55 -0300

    Adjustment

--- Backend/src/Index.js
+++ Backend/src/Index.js
@@ -13,6 +13,23 @@ app.use(
     credentials: true,
   })
 );
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://kuglerstore.vercel.app"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  next();
+});
+
+app.options("*", cors());
+
 app.use(cookieparser());
 app.use(router);
 
